28da21b594aaa398690c8c76948b58bb5a4e5b1b
Faris Alkassim <faruka16@gmail.com>
2024-08-03 11:44:31 -0400

    Update HW_Lesson6

--- pages/search_results_page.py
+++ pages/search_results_page.py
@@ -10,6 +10,10 @@ class SearchResultsPage(Page):
 
     FAVORITES_BTN = (By.CSS_SELECTOR, "[data-test='FavoritesButton']")
     FAVORITES_TOOLTIP_TXT = (By.XPATH, "//*[text()='Click to sign in and save']")
+    LISTINGS = (By.CSS_SELECTOR, "[data-test='@web/site-top-of-funnel/ProductCardWrapper']")
+    PRODUCT_TITLE = (By.CSS_SELECTOR, "[data-test='product-title']")
+    PRODUCT_IMG = (By.CSS_SELECTOR, 'img')
+
 
     def hover_fav_icon(self):
         # fav_icon = self.find_element(*self.FAVORITES_BTN)
@@ -27,3 +31,16 @@ class SearchResultsPage(Page):
 
     def verify_product_in_url(self, expected_product):
         self.verify_partial_url(expected_product)
+
+    def verify_product_name_img(self):
+        self.driver.execute_script("window.scrollBy(0,2000)", "")
+        sleep(4)
+        self.driver.execute_script("window.scrollBy(0,2000)", "")
+
+        all_products = self.driver.find_elements(*self.LISTINGS)  # [WebEl1, WebEl2, WebEl3, WebEl4]
+
+        for product in all_products:
+            title = product.find_element(*self.PRODUCT_TITLE).text
+            assert title, 'Product title not shown'
+            print(title)
+            product.find_element(*self.PRODUCT_IMG)
